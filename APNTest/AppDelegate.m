fe494110cc40334c054de3325250abf15d2dca6b
ChungYeul In <sldk86@naver.com>
2014-01-20 17:21:56 +0900

    p39 실습입니다.
    p39 실습입니다.

--- APNTest/AppDelegate.m
+++ APNTest/AppDelegate.m
@@ -7,12 +7,14 @@
 //
 
 #import "AppDelegate.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    UIRemoteNotificationType notiType = UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound;
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notiType];
     return YES;
 }
 							
@@ -41,6 +43,32 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"Success to register APN : %@", deviceToken);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Fail to register APN : %@", [error localizedDescription]);
+}
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"remote notification : %@", [userInfo description]);
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    
+    NSString *alert = [apsInfo objectForKey:@"alert"];
+    NSLog(@"Received Push Alert : %@", alert);
+    
+    NSString *sound = [apsInfo objectForKey:@"sound"];
+    NSLog(@"Received Push Sound : %@", sound);
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+    NSString *badge = [apsInfo objectForKey:@"badge"];
+    NSLog(@"Received Push Badge : %@", badge);
+    application.applicationIconBadgeNumber = [[apsInfo objectForKey:@"badge"] integerValue];
+    
+    UIAlertView *myAlert = [[UIAlertView alloc] initWithTitle:@"메세지" message:alert delegate:nil cancelButtonTitle:nil otherButtonTitles:@"확인", nil];
+    [myAlert show];
 }
 
 @end
